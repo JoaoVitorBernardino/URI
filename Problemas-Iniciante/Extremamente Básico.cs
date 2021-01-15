6f6a113c6c949acefa413ceecf3e1e9b79443d63
João Vitor L. Bernardino <65207435+JoaoVitorBernardino@users.noreply.github.com>
2021-01-15 11:46:46 -0300

    Extremamente Básico
    Link da questão: https://www.urionlinejudge.com.br/judge/pt/problems/view/1001

---
+++ Problemas-Iniciante/Extremamente Básico.cs
@@ -0,0 +1,15 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+       int A = int.Parse(Console.ReadLine());   
+       int B = int.Parse(Console.ReadLine());
+       
+       int X = A + B;
+       
+       Console.WriteLine("X = " + X);
+    }
+
+}
