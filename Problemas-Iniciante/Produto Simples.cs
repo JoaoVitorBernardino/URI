6377970229d1c710e48b111d59674780c16122d8
João Vitor L. Bernardino <65207435+JoaoVitorBernardino@users.noreply.github.com>
2021-01-15 11:57:54 -0300

    Produto Simples
    Link da questão: https://www.urionlinejudge.com.br/judge/pt/problems/view/1004

---
+++ Problemas-Iniciante/Produto Simples.cs
@@ -0,0 +1,15 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int A = int.Parse(Console.ReadLine());
+        int B = int.Parse(Console.ReadLine());
+    
+        int PROD = A * B;
+    
+        Console.WriteLine("PROD = " + PROD);
+    }
+
+}
