92c4bf7196646851ba673a56b97ee081429553fa
João Vitor L. Bernardino <65207435+JoaoVitorBernardino@users.noreply.github.com>
2021-01-15 11:55:49 -0300

    Soma Simples
    Link da questão: https://www.urionlinejudge.com.br/judge/pt/problems/view/1003

---
+++ Problemas-Iniciante/Soma Simples.cs
@@ -0,0 +1,14 @@
+using System;
+
+class URI 
+{
+    static void Main(string[] args)
+    {
+        int A = int.Parse(Console.ReadLine());
+        int B = int.Parse(Console.ReadLine());
+    
+        int SOMA = A + B;
+    
+        Console.WriteLine("SOMA = " + SOMA);
+    }
+}
