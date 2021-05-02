a6039615c3f0b073f59c5e89454c187744b9ea9e
João Vitor L. Bernardino <65207435+JoaoVitorBernardino@users.noreply.github.com>
2021-05-02 15:46:29 -0300

    Create 1016 - Distância.cs

---
+++ Problemas-Iniciante/1016 - Distância.cs
@@ -0,0 +1,15 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int km = int.Parse(Console.ReadLine());
+        
+        int tempo = km * 2;
+        
+        Console.WriteLine(tempo + " minutos");
+
+    }
+
+}
