c808beaf5ed3c8e4941284f1935aa5d99c4cfddb
João Vitor L. Bernardino <65207435+JoaoVitorBernardino@users.noreply.github.com>
2021-10-22 16:05:28 -0300

    Create 1759 - Ho Ho Ho.c

---
+++ Problemas-Iniciante/1759 - Ho Ho Ho.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int N, i;
+    
+    scanf("%d", &N);
+    
+    while(i != N - 1) {
+        printf("Ho ");
+        i++;
+    }
+    
+    printf("Ho!\n");
+    
+    return 0;
+}
